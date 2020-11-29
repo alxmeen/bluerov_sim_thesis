7ef91477cfb46145df594224f056d35666f29b33
NBauschmann <nathalie_bauschmann@hotmail.de>
2020-11-29 12:59:47 +0100

    basic functioniality ranges plugin, still work in progress

--- include/gazebo_range_sensor_plugin.h
+++ include/gazebo_range_sensor_plugin.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <ros/ros.h>
+#include <range_sensor/RangeMeasurement.h>
+#include <range_sensor/RangeMeasurementArray.h>
 
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/gazebo.hh>
@@ -7,21 +9,25 @@
 #include <ignition/math.hh>
 #include <sdf/sdf.hh>
 
+
 namespace gazebo {
 static constexpr auto kDefaultPubRate = 7.0;
 static constexpr auto kDefaultRangesTopic = "tag_detections_sim";
-static constexpr auto kDefaultRangesNoise = 100.0;    
-}
+static constexpr auto kDefaultRangesNoise = 0.1;
+static constexpr auto kDefaultFov = 70;
+static constexpr auto kDefaultDropProb = 0.05;
 
-class RangesPlugin : public SensorPlugin {
+
+
+class RangesPlugin : public ModelPlugin {
     public: 
      RangesPlugin();
      virtual ~RangesPlugin();
 
     protected:
-     virtual void Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf);
+     virtual void Load(physics::ModelPtr model, sdf::ElementPtr sdf);
      virtual void OnUpdate(const common::UpdateInfo &);
-     void gedSdfParams(sdf::ElementPtr sdf);
+     void getSdfParams(sdf::ElementPtr sdf);
 
     private:
      std::string namespace_;
@@ -30,6 +36,8 @@ class RangesPlugin : public SensorPlugin {
      event::ConnectionPtr update_connection_;
      std::string ranges_topic_;
      double ranges_noise_;
+     double max_angle_;
+     double drop_prob_;
 
      ros::NodeHandle *node_handle_;
      ros::Publisher ranges_pub_;
@@ -37,16 +45,18 @@ class RangesPlugin : public SensorPlugin {
      double pub_rate_;
 
      std::default_random_engine random_generator_;
-     std::normal_distribution<doubl> standard_normal_distribution_;
+     std::normal_distribution<double> standard_normal_distribution_;
+     std::uniform_real_distribution<double> uniform_real_distribution_;
 
      common::Time last_pub_time_;
      common::Time last_time_;
 
-     ignition::maths::Vector3d pos_tag_1_;
-     ignition::maths::Vector3d pos_tag_2_;
-     ignition::maths::Vector3d pos_tag_3_;
-     ignition::maths::Vector3d pos_tag_4_;
-     
+     ignition::math::Vector3d pos_tag_1_;
+     ignition::math::Vector3d pos_tag_2_;
+     ignition::math::Vector3d pos_tag_3_;
+     ignition::math::Vector3d pos_tag_4_;
 
-    
+     bool initialized_;
+     
+};
 } // namespace gazebo
--- plugins/gazebo_range_sensor_plugin.cpp
+++ plugins/gazebo_range_sensor_plugin.cpp
@@ -4,45 +4,47 @@ namespace gazebo
 {
     GZ_REGISTER_MODEL_PLUGIN(RangesPlugin)
 
-    RangesPlugin::RangesPlugin() : SensorPlugin()
+    RangesPlugin::RangesPlugin() : ModelPlugin() {}
 
-                                       RangesPlugin::~RangesPlugin()
-    {
-        update_connection_->~Connection();
-    }
+    RangesPlugin::~RangesPlugin() { update_connection_->~Connection(); }
 
-    void RangesPlugin::getSdfParams(sed::ElementPtr sdf)
-    {
+    void RangesPlugin::getSdfParams(sdf::ElementPtr sdf) {
         namespace_.clear();
-        if (sdf->HasElement("robotNamespace"))
-        {
+        if (sdf->HasElement("robotNamespace")) {
             namespace_ = sdf->GetElement("robotNamespace")->Get<std::string>();
         }
-        if (sdf->HasElement("pubRate"))
-        {
+        if (sdf->HasElement("pubRate")) {
             pub_rate_ = sdf->GetElement("pubRate")->Get<double>();
-        }
-        else
-        {
+        } else {
             pub_rate_ = kDefaultPubRate;
             gzwarn << "[ranges_plugin] Using default publication rate of "
                    << pub_rate_ << "Hz\n";
         }
-        if (sdf->HasElement("rangesTopic"))
-        {
-            ranges_topic_ = sdf->GetElement("rangesTopic")->Get<std::string>();
+        if (sdf->HasElement("rangesTopic")) {
+            ranges_topic_ = sdf->GetElement("rangesTopic")->Get<std::string>(); 
+        } else {
+            ranges_topic_ = kDefaultRangesTopic; 
         }
-        else
-        {
-            ranges_topic_ = kDefaultRangesTopic;
-        }
-        if (sdf->HasElement("noise"))
-        {
+        if (sdf->HasElement("noise")) {
             ranges_noise_ = sdf->GetElement("noise")->Get<double>();
-        }
-        else
-        {
+        } else {
             ranges_noise_ = kDefaultRangesNoise;
+            gzwarn << "[ranges_plugin] Using default noise "
+                   << ranges_noise_ << "\n";
+        }
+        if (sdf->HasElement("fov")) {
+            // we'll check visibility using half the viewing angle
+            max_angle_ = sdf->GetElement("fov")->Get<double>()/2.0;
+        } else {
+            max_angle_ = kDefaultFov/2.0;
+            gzwarn << "[ranges_plugin] Using default viewing angle "
+                   << kDefaultFov << "\n"; } 
+        if (sdf->HasElement("dropProb")) {
+            drop_prob_ = sdf->GetElement("dropProb")->Get<double>();
+        } else {
+            drop_prob_ = kDefaultDropProb;
+            gzwarn << "[ranges_plugin] Using default probability "
+                   << kDefaultDropProb << " for dropping measurements\n";
         }
     }
 
@@ -56,7 +58,7 @@ namespace gazebo
 
         if (!ros::isInitialized())
         {
-            ROS_FATAL_STREAM("Ros node for gazebo not initialized");
+            ROS_FATAL_STREAM("ROS node for gazebo not initialized");
             return;
         }
         node_handle_ = new ros::NodeHandle(namespace_);
@@ -66,11 +68,8 @@ namespace gazebo
 
         ranges_pub_ =
             node_handle_->advertise<range_sensor::RangeMeasurementArray>(ranges_topic_, 1);
-
-        pos_tag_1_ = world_->ModelByName("tag_1")->RelativePose().Pos()
-        pos_tag_2_ = world_->ModelByName("tag_2")->RelativePose().Pos()
-        pos_tag_3_ = world_->ModelByName("tag_3")->RelativePose().Pos()
-        pos_tag_4_ = world_->ModelByName("tag_4")->RelativePose().Pos()
+        
+        initialized_ = false;
     }
 
     void RangesPlugin::OnUpdate(const common::UpdateInfo &)
@@ -78,40 +77,108 @@ namespace gazebo
         common::Time current_time = world_->SimTime();
         double dt = (current_time - last_pub_time_).Double();
 
-        if (dt > 1.0 / pub_rate_)
+        if (initialized_ == false) {
+            if (world_->ModelByName("apriltag_tank")->GetChildLink("tag_1::base_link")) {
+                pos_tag_1_ = world_->ModelByName("apriltag_tank")->GetChildLink("tag_1::base_link")->RelativePose().Pos();
+                gzmsg << "[ranges plugin] Tag 1 Position found.\n";
+            } else {
+                gzwarn << "[ranges_plugin] Tag 1 Position not found.\n";
+            }
+            if (world_->ModelByName("apriltag_tank")->GetChildLink("tag_2::base_link")) {
+                pos_tag_2_ = world_->ModelByName("apriltag_tank")->GetChildLink("tag_2::base_link")->RelativePose().Pos();
+                gzmsg << "[ranges plugin] Tag 2 Position found.\n";
+            } else {
+                gzwarn << "[ranges_plugin] Tag 2 Position not found.\n";
+            }
+            if (world_->ModelByName("apriltag_tank")->GetChildLink("tag_3::base_link")) {
+                pos_tag_3_ = world_->ModelByName("apriltag_tank")->GetChildLink("tag_3::base_link")->RelativePose().Pos();
+                gzmsg << "[ranges plugin] Tag 3 Position found.\n";
+            } else {
+                gzwarn << "[ranges_plugin] Tag 3 Position not found.\n";
+            }
+            if (world_->ModelByName("apriltag_tank")->GetChildLink("tag_4::base_link")) {
+                pos_tag_4_ = world_->ModelByName("apriltag_tank")->GetChildLink("tag_4::base_link")->RelativePose().Pos();
+                gzmsg << "[ranges plugin] Tag 4 Position found.\n";
+                initialized_ = true;
+            } else {
+                gzwarn << "[ranges_plugin] Tag 4 Position not found.\n";
+            }
+        }
+
+        if (dt > 1.0 / pub_rate_ && initialized_ == true)
         {
-            range_sensor::RangeMeasurementArray msg;
+            range_sensor::RangeMeasurementArray msg_array;
+            msg_array.header.stamp = ros::Time::now();
+            msg_array.header.frame_id = "map";
 
             // get world pose, orientation
             ignition::math::Vector3d pos_sensor =
                 model_->GetLink("range_sensor_link")->WorldPose().Pos();
 
-            ignition::math::Vector3d x_axis = model_->GetLink("range_sensor_link")->WorldPose().Rot().RotateVector(Vector3d(1.0, 0.0, 0.0));
+            ignition::math::Vector3d x_unit_vector(1.0, 0.0, 0.0);
+            ignition::math::Vector3d body_x_axis = model_->GetLink("range_sensor_link")->WorldPose().Rot().RotateVector(x_unit_vector);
             
-            // if tag within cone, compute distance to this tag
-            ignition::math::Vector3d sensor_to_tag_1 = pose_tag_1_ - pos_sensor;
-            double angle_tag_1 = acos(sensor_to_tag_1.Dot(x_axis)) / (sensor_to_tag_1.Length() * x_axis.Length());
+            // get viewing angle of tag
+            ignition::math::Vector3d sensor_to_tag_1 = pos_tag_1_ - pos_sensor;
+            double angle_tag_1 = acos(sensor_to_tag_1.Dot(body_x_axis)) / (sensor_to_tag_1.Length() * body_x_axis.Length());
             
-
+            // probability that measurement will be dropped
+            double p1 = standard_normal_distribution_(random_generator_);
+            if (angle_tag_1 < max_angle_ && p1 >= drop_prob_) {
+                range_sensor::RangeMeasurement msg;
+                msg.header.stamp = ros::Time::now();
+                msg.header.frame_id = "map";
+                msg.id = 1;
+                // still need to add noise
+                msg.range = sensor_to_tag_1.Length();
+                msg_array.measurements.push_back(msg);
+            }
             
-            ignition::math::vector3d sensor_to_tag_2 = pose_tag_2_ - pos_sensor;
-            ignition::math::vector3d sensor_to_tag_3 = pose_tag_3_ - pos_sensor;
-            ignition::math::vector3d sensor_to_tag_4 = pose_tag_4_ - pos_sensor;
-
+            ignition::math::Vector3d sensor_to_tag_2 = pos_tag_2_ - pos_sensor;
+            double angle_tag_2 = acos(sensor_to_tag_2.Dot(body_x_axis)) / (sensor_to_tag_2.Length() * body_x_axis.Length());
             
+            // probability that measurement will be dropped
+            double p2 = standard_normal_distribution_(random_generator_);
+            if (angle_tag_2 < max_angle_ && p2 >= drop_prob_) {
+                range_sensor::RangeMeasurement msg;
+                msg.header.stamp = ros::Time::now();
+                msg.header.frame_id = "map";
+                msg.id = 2;
+                // still need to add noise
+                msg.range = sensor_to_tag_2.Length();
+                msg_array.measurements.push_back(msg);
+            }
 
-            // there should be a certain probability that this distance measurement
-            // is dropped, this could be correlated to distance to tag
+            ignition::math::Vector3d sensor_to_tag_3 = pos_tag_3_ - pos_sensor;
+            double angle_tag_3 = acos(sensor_to_tag_3.Dot(body_x_axis)) / (sensor_to_tag_3.Length() * body_x_axis.Length());
+            
+            // probability that measurement will be dropped
+            double p3 = standard_normal_distribution_(random_generator_);
+            if (angle_tag_3 < max_angle_ && p3 >= drop_prob_) {
+                range_sensor::RangeMeasurement msg;
+                msg.header.stamp = ros::Time::now();
+                msg.header.frame_id = "map";
+                msg.id = 3;
+                // still need to add noise
+                msg.range = sensor_to_tag_3.Length();
+                msg_array.measurements.push_back(msg);
+            }
 
-            // if not dropped, fill out RangeMeasurement msg
-
-            // repeat this for other three tags, can be hardcoded
-
-            // fill RangeMeasurementArray msg
-
-            // publish this
-
-            ranges_pub_.publish(msg);
+            ignition::math::Vector3d sensor_to_tag_4 = pos_tag_4_ - pos_sensor;
+            double angle_tag_4 = acos(sensor_to_tag_4.Dot(body_x_axis)) / (sensor_to_tag_4.Length() * body_x_axis.Length());
+            
+            double p4 = standard_normal_distribution_(random_generator_);
+            if (angle_tag_4 < max_angle_ && p4 >= drop_prob_) {
+                range_sensor::RangeMeasurement msg;
+                msg.header.stamp = ros::Time::now();
+                msg.header.frame_id = "map";
+                msg.id = 4;
+                // still need to add noise
+                msg.range = sensor_to_tag_4.Length();
+                msg_array.measurements.push_back(msg);
+            }
+            
+            ranges_pub_.publish(msg_array);
             last_pub_time_ = current_time;
         }
     }
